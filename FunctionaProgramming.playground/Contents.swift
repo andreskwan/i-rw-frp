821e17d9e5775c3a7a913138f7b94d021846c44a
Andres Kwan <andres.x.kwan.-nd@disney.com>
2016-08-22 15:52:52 -0500

    [exp1]-[Imperative approach]-[find even numbers in a range of ints]

---
+++ FunctionaProgramming.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var evens = [Int]()
+for i in 1...10 {
+    if i % 2 == 0 {
+        evens.append(i)
+    }
+}
+print(evens)
+
