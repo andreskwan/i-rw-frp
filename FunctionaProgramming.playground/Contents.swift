c6bb741e2b902fa531ac8a1f4306432bedd8f2ff
Andres Kwan <andres.x.kwan.-nd@disney.com>
2016-08-22 16:32:25 -0500

    [exp1]-[FRP]-[Difference between filter & map]

--- FunctionaProgramming.playground/Contents.swift
+++ FunctionaProgramming.playground/Contents.swift
@@ -14,6 +14,13 @@ print(evens)
 func isEven(number: Int) -> Bool {
     return number % 2 == 0
 }
-evens = Array(1...10).filter(isEven)
-evens = Array(1...10).filter{$0 % 2 == 0}
-print(evens)
+
+// difference between map and filter 
+let evens1 = Array(1...10).filter(isEven)
+let evens2 = Array(1...10).filter{$0 % 2 == 0}
+let evens3 = Array(1...10).map{$0 % 2}
+
+print(evens1)
+print(evens2)
+print(evens3)
+    
